00d869bf7491a298c99b2a76dc66cb756eb10944
bchoward3 <bchoward@wisc.edu>
2026-02-08 15:56:23 -0500

    Activity 5
    Activity 5.

---
+++ js/adaptedTutorial.js
@@ -0,0 +1,69 @@
+/* Map of GeoJSON data from MegaCities.geojson */
+
+//declare map var in global scope
+var map;
+
+//function to instantiate the Leaflet map
+function createMap(){
+
+    //create the map
+    map = L.map('map', {
+        center: [20, 0],
+        zoom: 2
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+    //call getData function
+    getData();
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+
+    fetch("data/MegaCities.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+
+            //circle marker style
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            //create GeoJSON layer
+            L.geoJson(json, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                },
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        });
+}
+
+//function to attach popups to each feature
+function onEachFeature(feature, layer) {
+
+    var popupContent = "";
+
+    if (feature.properties) {
+        for (var property in feature.properties) {
+            popupContent += 
+                "<p><strong>" + property + "</strong>: " +
+                feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    }
+}
+
+//load map once the page is ready
+document.addEventListener('DOMContentLoaded', createMap);
--- js/main.js
+++ js/main.js
@@ -1,133 +1,34 @@
-// Add all scripts to the JS folder
+// Declare the map variable with setView method defining map center (Denver approx.) and zoom level.
+var map;
 
-var map = L.map('map').setView([40, -104], 5);
+// Create the map function
+function createMap() {
+    // Create the map
+    map = L.map('map').setView([32, -81], 9);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    // Add the Alidade Smooth Dark base tilelayer to the map
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+        minZoom: 0,
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        ext: 'png'
+    }).addTo(map);
 
-var myLines = [{
-  "type": "LineString",
-  "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-}, {
-  "type": "LineString",
-  "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-}];
-
-var myStyle = {
-  "color": "#ff7800",
-  "weight": 5,
-  "opacity": 0.65
-};
-
-L.geoJSON(myLines, {
-  style: myStyle
-}).addTo(map);
-
-var states = [{
-  "type": "Feature",
-  "properties": { "party": "Republican" },
-  "geometry": {
-    "type": "Polygon",
-    "coordinates": [[
-      [-104.05, 48.99],
-      [-97.22, 48.98],
-      [-96.58, 45.94],
-      [-104.03, 45.94],
-      [-104.05, 48.99]
-    ]]
-  }
-}, {
-  "type": "Feature",
-  "properties": { "party": "Democrat" },
-  "geometry": {
-    "type": "Polygon",
-    "coordinates": [[
-      [-109.05, 41.00],
-      [-102.06, 40.99],
-      [-102.03, 36.99],
-      [-109.04, 36.99],
-      [-109.05, 41.00]
-    ]]
-  }
-}];
-
-L.geoJSON(states, {
-  style: function (feature) {
-    switch (feature.properties.party) {
-      case 'Republican': return { color: "#ff0000" };
-      case 'Democrat':   return { color: "#0000ff" };
-    }
-  }
-}).addTo(map);
-
-
-var geojsonMarkerOptions = {
-  radius: 8,
-  fillColor: "#ff7800",
-  color: "#000",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 0.8
-};
-
-var someGeojsonFeature = {
-  "type": "Feature",
-  "properties": {
-    "name": "Coors Field",
-    "amenity": "Baseball Stadium",
-    "popupContent": "This is where the Rockies play!"
-  },
-  "geometry": {
-    "type": "Point",
-    "coordinates": [-104.99404, 39.75621]
-  }
-};
-
-L.geoJSON(someGeojsonFeature, {
-  pointToLayer: function (feature, latlng) {
-    return L.circleMarker(latlng, geojsonMarkerOptions);
-  }
-}).addTo(map);
-
-
-function onEachFeature(feature, layer) {
-  if (feature.properties && feature.properties.popupContent) {
-    layer.bindPopup(feature.properties.popupContent);
-  }
+    // Call the function to load GeoJSON data
+    getData();
 }
 
-L.geoJSON(someGeojsonFeature, {
-  onEachFeature: onEachFeature
-}).addTo(map);
+// Function to retrieve the GeoJSON data and add it to the map
+function getData() {
+    fetch("data/Chatham_municipalities.geojson")
+        .then(function(response) {
+            return response.json();  // Parse the JSON data
+        })
+        .then(function(json) {
+            // Create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json).addTo(map);
+        });
+}
 
-
-var someFeatures = [{
-  "type": "Feature",
-  "properties": {
-    "name": "Coors Field",
-    "show_on_map": true
-  },
-  "geometry": {
-    "type": "Point",
-    "coordinates": [-104.99404, 39.75621]
-  }
-}, {
-  "type": "Feature",
-  "properties": {
-    "name": "Busch Field",
-    "show_on_map": false
-  },
-  "geometry": {
-    "type": "Point",
-    "coordinates": [-104.98404, 39.74621]
-  }
-}];
-
-L.geoJSON(someFeatures, {
-  filter: function (feature) {
-    return feature.properties.show_on_map;
-  }
-}).addTo(map);
-
+// Call createMap() when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', createMap);
