c38637383af7f08dfcd923101aa0568b22f0b491
bchoward3 <bchoward@wisc.edu>
2026-02-13 23:06:38 -0500

    Proportional symbols.
    Proportional symbols added (geoJSON adjusted for column names).

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Declare the map variable with setView method defining map center (Denver approx.) and zoom level.
 var map;
+var minValue;
 
 // Create the map function
 function createMap() {
@@ -19,17 +20,80 @@ function createMap() {
     getData();
 }
 
-// Function to retrieve the GeoJSON data and add it to the map
-function getData() {
-    fetch("data/Chatham_municipalities.geojson")
-        .then(function(response) {
-            return response.json();  // Parse the JSON data
-        })
-        .then(function(json) {
-            // Create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json).addTo(map);
-        });
+function calculateMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var City of data.features){
+        //loop through each year
+        for(var year = 2016; year <= 2024; year+=5){
+              //get population for current year
+              var value = City.properties["Pop_"+ String(year)];
+              //add value to array
+              allValues.push(value);
+        }
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
 }
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data){
+
+    //Step 4: Determine which attribute to visualize with proportional symbols
+    var attribute = "Pop_2016";
+
+    //create marker options
+    var geojsonMarkerOptions = {
+        fillColor: "#ff7800",
+        color: "#fff",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+        radius: 8
+    };
+
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            //Step 5: For each feature, determine its value for the selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            //Step 6: Give each feature's circle marker a radius based on its attribute value
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+            //create circle markers
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
+
+// Function to retrieve the GeoJSON data and add it to the map
+function getData(){
+    //load the data
+    fetch("data/Chatham_municipalities.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //calculate minimum data value
+            minValue = calculateMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json);
+        })
+};
 
 // Call createMap() when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', createMap);
